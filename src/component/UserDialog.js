7bde970d51ed801d2d15bc501c234cac33d4e687
Flash <188813950@qq.com>
2019-11-19 03:34:11 +0800

    消除「需要 onChange」的警告

--- src/component/UserDialog.js
+++ src/component/UserDialog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './style/UserDialog.css'
-import {signUp} from '../leanCloud'
+import { signUp } from '../leanCloud'
 
 export default class UserDialog extends Component {
   constructor(props) {
@@ -18,22 +18,22 @@ export default class UserDialog extends Component {
       selected: e.target.value
     })
   }
-  signUp(e){
+  signUp(e) {
     e.preventDefault()
-    let {username, password} = this.state.formData
-    let success = (user)=>{
+    let { username, password } = this.state.formData
+    let success = (user) => {
       this.props.onSignUp.call(null, user)
     }
-    let error = (error)=>{
+    let error = (error) => {
       console.log(error)
     }
     signUp(username, password, success, error)
   }
   signIn(e) { }
-  changeFormData = (e,key) =>{
+  changeFormData = (e, key) => {
     let stateCopy = JSON.parse(JSON.stringify(this.state))  // 用 JSON 深拷贝
     stateCopy.formData[key] = e.target.value
-    this.setState(stateCopy,()=>console.log(this.state))
+    this.setState(stateCopy, () => console.log(this.state))
   }
   render() {
     let signUpForm = (
@@ -41,12 +41,12 @@ export default class UserDialog extends Component {
         <div className="row">
           <label>用户名</label>
           <input type="text" value={this.state.formData.username}
-            onChange={(e)=>this.changeFormData(e,'username')} />
+            onChange={(e) => this.changeFormData(e, 'username')} />
         </div>
         <div className="row">
           <label>密码</label>
           <input type="password" value={this.state.formData.password}
-            onChange={(e)=>this.changeFormData(e,'password')} />
+            onChange={(e) => this.changeFormData(e, 'password')} />
         </div>
         <div className="row actions">
           <button type="submit">注册</button>
@@ -58,12 +58,12 @@ export default class UserDialog extends Component {
         <div className="row">
           <label>用户名</label>
           <input type="text" value={this.state.formData.username}
-            onChange={(e)=>this.changeFormData(e,'username')} />
+            onChange={(e) => this.changeFormData(e, 'username')} />
         </div>
         <div className="row">
           <label>密码</label>
           <input type="password" value={this.state.formData.password}
-            onChange={(e)=>this.changeFormData(e,'password')} />
+            onChange={(e) => this.changeFormData(e, 'password')} />
         </div>
         <div className="row actions">
           <button type="submit">登录</button>
@@ -73,17 +73,17 @@ export default class UserDialog extends Component {
     return (
       <div className="UserDialog-Wrapper">
         <div className="UserDialog">
-          <div onChange={this.switch.bind(this)}>
-            <label><input
-              type="radio"
-              value="signUp"
-              name='xxx'
-              checked={this.state.selected === 'signUp'} /> 注册</label>
-            <label><input
-              type="radio"
-              value="signIn"
-              name='xxx'
-              checked={this.state.selected === 'signIn'} /> 登录 </label>
+          <div>
+            <label>
+              <input type="radio" value="signUp"
+                checked={this.state.selected === 'signUp'}
+                onChange={this.switch.bind(this)}
+              /> 注册</label>
+            <label>
+              <input type="radio" value="signIn"
+                checked={this.state.selected === 'signIn'}
+                onChange={this.switch.bind(this)}
+              /> 登录</label>
           </div>
           <div className="panes">
             {this.state.selected === 'signUp' ? signUpForm : signInForm}
