fc83784ce75886365289e1e6d47e429d06e94379
Flash <188813950@qq.com>
2019-11-18 16:51:36 +0800

    fex value

--- src/component/TodoInput.js
+++ src/component/TodoInput.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 export default class TodoInput extends Component {
   render() {
-    return <input type="text" value={this.props.content} />
+    return (
+      return <input type="text" defaultValue={this.props.content}/>
+    )
   }
 }
