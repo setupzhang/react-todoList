44f9ef3fcf7f21d8761d0e935704e86472d7be36
Flash <188813950@qq.com>
2019-11-18 16:19:52 +0800

    骨架

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import "normalize.css"
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      newTodo: 'test',
+      todoList: [
+        { id: 1, title: '第一个待办' }
+      ]
+    }
+  }
+  render() {
 
-    </div>
-  );
+    let todos = this.state.todoList.map((item, index) => {
+      return <li>{item.title}</li>
+    })
+    return (
+      <div className="App">
+        <h1>我的待办</h1>
+        <div className="inputWrapper">
+          <input type="text" value={this.state.newTodo} />
+        </div>
+        <ol>
+          {todos}
+        </ol>
+      </div>
+    )
+  }
 }
 
 export default App;
