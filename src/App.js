86db0debc06a148d024374181efe394046d9b0e6
Flash <188813950@qq.com>
2019-11-18 16:24:23 +0800

    将输入框变成input组件

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import "normalize.css"
 import './App.css'
+import TodoInput from './component/TodoInput'
 
 class App extends Component {
   constructor(props) {
@@ -21,7 +22,7 @@ class App extends Component {
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputWrapper">
-          <input type="text" value={this.state.newTodo} />
+          <TodoInput content={this.state.newTodo} />
         </div>
         <ol>
           {todos}
