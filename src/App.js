b0567a4b8b1c48f776b7f2551b23149420dd93b4
Flash <188813950@qq.com>
2019-11-18 22:59:05 +0800

    添加删除功能

--- src/App.js
+++ src/App.js
@@ -12,8 +12,12 @@ class App extends Component {
       todoList: []
     }
   }
-  toggle(e, todo){
+  toggle(e, todo) {
     todo.status = todo.status === 'completed' ? '' : 'completed'
+    this.setState(this.state)
+  }
+  delete(event, todo){
+    todo.deleted = true
     this.setState(this.state) 
   }
   changeTitle(event) {
@@ -36,12 +40,14 @@ class App extends Component {
   }
   render() {
 
-    let todos = this.state.todoList.map((item, index) => {
+    let todos = this.state.todoList
+      .filter((item)=> !item.deleted)
+      .map((item,index)=>{
       return (
         <li key={index}>
           <TodoItem todo={item}
             onToggle={this.toggle.bind(this)}
-          />
+            onDelete={this.delete.bind(this)} />
         </li>
       )
     })
